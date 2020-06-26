c9709af813e0e42cd975d165c364e2c4ecfb171e
Srinivasa Rao <srinivasarao.rao@gmail.com>
2020-06-26 09:43:09 +0100

    fix conf.py to make rtd work

--- conf.py
+++ conf.py
@@ -55,3 +55,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# this is important for readthedocs to build succesfully (rtd is apparently using
+# a different version of sphinx
+master_doc = 'index'
